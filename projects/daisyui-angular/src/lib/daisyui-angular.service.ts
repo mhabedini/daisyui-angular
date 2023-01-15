5fc7079425abc43b419d0bd2b70c9f08d3e0d40b
Mohammad Hosein Abedini <m.h.abedini@gmail.com>
2023-01-15 19:12:18 +0330

    Install eslint

--- projects/daisyui-angular/src/lib/daisyui-angular.service.ts
+++ projects/daisyui-angular/src/lib/daisyui-angular.service.ts
@@ -3,7 +3,4 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class DaisyuiAngularService {
-
-  constructor() { }
-}
+export class DaisyuiAngularService {}
